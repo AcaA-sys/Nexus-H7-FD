a8cb052b3cf5864a8bf430aa0f88eed227bf842a
AcaA-sys <titanik71@yandex.com>
2026-04-01 14:37:06 +0300

    Update BeforePick.js

--- pnp_pneumatics/BeforePick.js
+++ pnp_pneumatics/BeforePick.js
@@ -1,1 +1,14 @@
+var P_LEVELS = {"0201":"920", "0402":"880", "1206":"650", "STRONG":"470", "DEFAULT":"750"};
+var nozzle = event.getNozzle();
+var partId = event.getPlacement().getPart().getId();
+var driver = machine.getDefaultDriver();
+var nozzleNum = nozzle.getName().match(/\d+/) || "1";
 
+if (nozzle.getPart() == null) {
+    var targetP = P_LEVELS["DEFAULT"];
+    if (partId.match(/0201/i)) targetP = P_LEVELS["0201"];
+    else if (partId.match(/0402|0603/i)) targetP = P_LEVELS["0402"];
+    else if (partId.match(/TQFP|BGA|QFP/i)) targetP = P_LEVELS["STRONG"];
+    
+    driver.sendCommand("M610 S1 P" + targetP + " N" + nozzleNum);
+}
